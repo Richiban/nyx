31bca36230840174ee304bf36e6090e955a8c308
Richard Gibson <richard@gibson.cx>
2026-02-11 10:41:42 +0000

    Create compiler folder

--- compiler/test_parray.fsx
+++
@@ -1,9 +0,0 @@
-open FParsec
-
-let testParser = parray 4 (pchar ' ' <|> pchar '\t')
-
-let test1 = run testParser "    hello"
-printfn "Test 1: %A" test1
-
-let test2 = run testParser "  hello"  
-printfn "Test 2: %A" test2
